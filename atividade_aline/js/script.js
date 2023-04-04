3cf4ba32b71196da85ac3b36bc8e68d63efe2096
Juliana <128002239+julianadlisboa@users.noreply.github.com>
2023-04-04 10:28:07 -0300

    Add files via upload

---
+++ atividade_aline/js/script.js
@@ -0,0 +1,38 @@
+function tlou(){
+	let tlouimg= document.querySelector("img#imagem");
+	let num= Math.trunc(Math.random()*10);
+	
+if(num== 0){
+	tlouing.setAttribute("scr", "img/maskgame.png");
+}
+else if(num== 1){
+	tlouimg.setAttribute("scr", "img/tlou1.png");
+}
+else if (num== 2){
+	tlouimg.setAttribute("scr", "img/tlou2.png");
+}
+else if(num== 3){
+	tlouimg.setAttribute("scr", "img/tlou3.webp");
+}
+else if(num==4){
+	tlouimg.setAttribute("scr", "img/tlou4.webp");
+}
+else if(num==5){
+	tlouimg.setAttribute("scr", "img/tlou5.png");
+}
+else if(num==6){
+	tlouimg.setAttribute("scr", "img/tlou6.png");
+}
+else if(num==7){
+	tlouimg.setAttribute("scr", "img/tlou7.jpg");
+}
+else if(num==8){
+	tlouimg.setAttribute("scr", "img/tlou8.png");
+}
+else if(num==9){
+	tlouimg.setAttribute("scr", "img/tlou9.jpeg");
+}
+else if(num==10){
+	tlouimg.setAttribute("scr", "img/tlou10.jpeg");
+}
+}
