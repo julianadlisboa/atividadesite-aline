66e52ce705119bae2d8ea50443aa579214ffa1f9
Juliana <128002239+julianadlisboa@users.noreply.github.com>
2023-04-04 17:41:27 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+function tomb(){
+	let tombimg= document.querySelector("img#imagem");
+	let num= Math.trunc(Math.random()*10);
+	
+if(num== 0){
+	tombimg.setAttribute("scr", "img/tomb.png");
+}
+else if(num== 1){
+	tomb2img.setAttribute("scr", "img/tomb2.png");
+}
+else if (num== 2){
+	tomb3img.setAttribute("scr", "img/tomb3.png");
+}
+else if(num== 3){
+	tomb4img.setAttribute("scr", "img/tomb4.png");
+}
+else if(num==4){
+	tomb5img.setAttribute("scr", "img/tomb5.png");
+}
+else if(num==5){
+	tomb6img.setAttribute("scr", "img/tomb6.png");
+}
+else if(num==6){
+	tomb7img.setAttribute("scr", "img/tomb7.png");
+}
+else if(num==7){
+	tomb8img.setAttribute("scr", "img/tomb8.png");
+}
+else if(num==8){
+	tomb9img.setAttribute("scr", "img/tomb9.png");
+}
+else if(num==9){
+	tomb10img.setAttribute("scr", "img/tomb10.png");
+}
+else if(num==10){
+	maskgameimg.setAttribute("scr", "img/maskgame.jpeg");
+}
+}
